3cc416f2bf85240dd868e6300ee9ca290f18dae7
moomiji <moomiji@users.noreply.github.com>
2024-05-08 08:00:35 +0000

    deploy: 6fc393e9e0717d0fcf9bab3a49ef4aaf7bfc02ca

---
+++ dir_d44c64559bbebec7f509842c48db8b23.js
@@ -0,0 +1,5 @@
+var dir_d44c64559bbebec7f509842c48db8b23 =
+[
+    [ "MaaFramework", "dir_370cd1367c21b1a87c5c9c6803b8d0b9.html", "dir_370cd1367c21b1a87c5c9c6803b8d0b9" ],
+    [ "MaaToolkit", "dir_229d1428d76e394aeaec36b75a5d5699.html", "dir_229d1428d76e394aeaec36b75a5d5699" ]
+];
--- search/defines_0.js
+++ search/defines_0.js
@@ -1,11 +1,11 @@
 var searchData=
 [
   ['maa_5fcontrol_5funit_5fapi_0',['MAA_CONTROL_UNIT_API',['../MaaPort_8h.html#a826aa33333d0528e8a238f2717265249',1,'MaaPort.h']]],
-  ['maa_5fdll_5fexport_1',['MAA_DLL_EXPORT',['../MaaPort_8h.html#a20cf214e76a3bf5d041786154513c892',1,'MaaPort.h']]],
-  ['maa_5fdll_5fimport_2',['MAA_DLL_IMPORT',['../MaaPort_8h.html#aed0af92192d8a7f6278afec3adfee80c',1,'MaaPort.h']]],
-  ['maa_5fdll_5flocal_3',['MAA_DLL_LOCAL',['../MaaPort_8h.html#a4be2819e9ef99d9befeefb94fa2afada',1,'MaaPort.h']]],
-  ['maa_5fframework_5fapi_4',['MAA_FRAMEWORK_API',['../MaaPort_8h.html#a67006334d91d228121dbba7a8bf8fe05',1,'MaaPort.h']]],
-  ['maa_5frpc_5fapi_5',['MAA_RPC_API',['../MaaPort_8h.html#a7c19101f6ca5fe6c8f217aa09a3f2863',1,'MaaPort.h']]],
+  ['maa_5fdeprecated_1',['MAA_DEPRECATED',['../MaaPort_8h.html#aea94eb63afa29e65c59844ec3f57d691',1,'MaaPort.h']]],
+  ['maa_5fdll_5fexport_2',['MAA_DLL_EXPORT',['../MaaPort_8h.html#a20cf214e76a3bf5d041786154513c892',1,'MaaPort.h']]],
+  ['maa_5fdll_5fimport_3',['MAA_DLL_IMPORT',['../MaaPort_8h.html#aed0af92192d8a7f6278afec3adfee80c',1,'MaaPort.h']]],
+  ['maa_5fdll_5flocal_4',['MAA_DLL_LOCAL',['../MaaPort_8h.html#a4be2819e9ef99d9befeefb94fa2afada',1,'MaaPort.h']]],
+  ['maa_5fframework_5fapi_5',['MAA_FRAMEWORK_API',['../MaaPort_8h.html#a67006334d91d228121dbba7a8bf8fe05',1,'MaaPort.h']]],
   ['maa_5ftoolkit_5fapi_6',['MAA_TOOLKIT_API',['../MaaPort_8h.html#a011c7bbd355fdfe6aa7599af52cc20ac',1,'MaaPort.h']]],
   ['maa_5futils_5fapi_7',['MAA_UTILS_API',['../MaaPort_8h.html#aabeb9919a6a22899b3e2e224c83e90db',1,'MaaPort.h']]],
   ['maaadbcontrollertype_5fkey_5fmask_8',['MaaAdbControllerType_Key_Mask',['../MaaDef_8h.html#a0aba12231a057e024b6f886d5ca57e42',1,'MaaDef.h']]],
